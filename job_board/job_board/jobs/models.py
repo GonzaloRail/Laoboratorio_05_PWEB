b52182d8a5c21de29be1b42a060f9699e5050a2f
Gonzalo01703 <glaymem@unsa.edu.pe>
2023-06-19 21:16:14 -0500

    Se creo el modeloi

--- job_board/job_board/jobs/models.py
+++ job_board/job_board/jobs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    requirements = models.TextField()
+    location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    employer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
