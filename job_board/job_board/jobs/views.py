bb961410427cf6459c17480bc92f695676958dab
Gonzalo01703 <glaymem@unsa.edu.pe>
2023-06-19 21:16:53 -0500

    Se creo views

--- job_board/job_board/jobs/views.py
+++ job_board/job_board/jobs/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Job
+
 
 # Create your views here.
+
+
+
+@login_required
+def job_list(request):
+    jobs = Job.objects.all()
+    return render(request, 'jobs/job_list.html', {'jobs': jobs})
+
+@login_required
+def job_detail(request, job_id):
+    job = Job.objects.get(pk=job_id)
+    # Aquí puedes agregar la lógica para manejar el envío de solicitudes de empleo
+    return render(request, 'jobs/job_detail.html', {'job': job})
